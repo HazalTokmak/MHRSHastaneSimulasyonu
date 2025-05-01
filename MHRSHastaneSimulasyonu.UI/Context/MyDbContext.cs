52b31856bce6594d72eb2845c3ef249e68a67b3b
HazalTokmak <tokmak.aysehazal@gmail.com>
2025-05-01 18:30:47 +0300

    Creates DbContext

--- MHRSHastaneSimulasyonu.UI/Context/MyDbContext.cs
+++ MHRSHastaneSimulasyonu.UI/Context/MyDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MHRSHastaneSimulasyonu.UI.Context
 {
-    public class MyDbContext
+    public class MyDbContext: DbContext
     {
     }
 }
